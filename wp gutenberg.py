bb7b9aeb78c807300e42e2e381e545337a9c84d5
masukgit <masuk1111@gmail.com>
2025-06-14 01:52:44 +0600

    updated

--- wp gutenberg.py
+++ wp gutenberg.py
@@ -7,16 +7,15 @@ wordpress_pass = '4SAR R06M D4GU OBLq u7Mt IXPy'
 token = base64.b64encode(f'{wordpress_user} : {wordpress_pass}'.encode()).decode('utf-8')
 headers = {
     'Authorization': f'Basic {token}',
-    'Content-Type': 'application/json',
-    'User-Agent': 'Firefox/5.0',
-    'status': 'draft'
+    'User-Agent': 'Firefox/5.0'
 }
 content = '<!-- wp:paragraph --><p>Mr. Younus is rejected to meet by Mr. Kiar Starmar, the prime minister of the people republic of England</p><!-- /wp:paragraph -->'
 data = {
     'title': 'Kiar Starmar is the player',
     'categories': '9',
     'slug': 'kiar-starmar-the-player',
-    'content': content
+    'content': content,
+    'status': 'draft'
 }
 
 res = post(api_url, headers=headers, json=data)
